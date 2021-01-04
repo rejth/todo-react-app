9fda92b954a42efe4cf16845d09a70cfb1c752da
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-01-04 21:42:24 +0300

    added components

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-// имя компонента с заглавной буквы
-// компонент - это функция которая возвращает (создает) html-шаблон
-// обращение к компоненту в JSX превращается в вызов метода React.createElement()
-const TodoList = () => {
-  const items = ['Drink coffee', 'Create Awesome App'];
-  return (
-    <ul>
-      <li>{ items[0] }</li>
-      <li>{ items[1] }</li>
-    </ul>
-  );
-}
-
-const AppHeader = () => {
-  return (
-    <h1>My To-Do List</h1>
-  );
-}
-
-const SearchPanel = () => {
-  return (
-    <input placeholder="search"></input>
-  );
-}
+import TodoList from './components/TodoList';
+import AppHeader from './components/AppHeader';
+import SearchPanel from './components/SearchPanel';
 
 const App = () => {
-  return (
-    <div>
-      <AppHeader/>
-      <SearchPanel/>
-      <TodoList/>
-    </div>
-  );
+	return (
+		<div>
+			<AppHeader/>
+			<SearchPanel/>
+			<TodoList/>
+		</div>
+	);
 }
 
-// первый аргумент метода ReactDom.render() принимает элемент, а не компонент
-// чтобы сделать из компонента элемента нужно прописать <.../>
 ReactDom.render(<App/>, document.getElementById('root'));
