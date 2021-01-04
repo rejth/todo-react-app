2674d0c3afc8f2266b02e9aaf408c3f0cef731e5
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-01-04 21:09:40 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// имя компонента с заглавной буквы
+// компонент - это функция которая возвращает (создает) html-шаблон
+// обращение к компоненту в JSX превращается в вызов метода React.createElement()
+const TodoList = () => {
+  const items = ['Drink coffee', 'Create Awesome App'];
+  return (
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const AppHeader = () => {
+  return (
+    <h1>My To-Do List</h1>
+  );
+}
+
+const SearchPanel = () => {
+  return (
+    <input placeholder="search"></input>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+}
+
+// первый аргумент метода ReactDom.render() принимает элемент, а не компонент
+// чтобы сделать из компонента элемента нужно прописать <.../>
+ReactDom.render(<App/>, document.getElementById('root'));
