4d70d6ae0bb53cc83968e81c064e02c00b3d265f
Ilia Kirsanov <kirsanovia@yandex.ru>
2021-01-04 22:42:39 +0300

    added more comments

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,14 +3,17 @@ import TodoListItem from './TodoListItem';
 
 const TodoList = ({ todos }) => {
 	const elements = todos.map(item => {
+		// * используем spread-оператор, чтобы передать в компонент новые переменные (свойства)
+		// * имена создаваемых свойств элемента и имена свойств объекта item одинаковые
+		// * поэтому в данном случаем удобно использовать spread-оператор
 		return (
-			<li><TodoListItem
-				label={ item.label }
-				important={ item.important }/>
+			<li>
+				<TodoListItem { ...item }/>
 			</li>
 		)
 	})
 
+	// * передаем в список ul массив JSX элементов elements
 	return (
 		<ul>
 			{ elements }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import TodoList from './components/TodoList';
 import AppHeader from './components/AppHeader';
 import SearchPanel from './components/SearchPanel';
 
+// * имя компонента должно быть с заглавной буквы, иначе Babel не поймет, что создается кастомный компонент
 const App = () => {
 	const todoData = [
 		{ label: 'Drink Coffee', important: false },
 		{ label: 'Have a lunch', important: false },
 		{ label: 'Build React App', important: true },
 	];
-
+// * react-компонент - это функция, которая всегда возвращает (создает) html-шаблон
 	return (
+// * обращение к компоненту в JSX превращается эквивалентно вызову метода React.createElement()
 		<div>
 			<AppHeader/>
 			<SearchPanel/>
@@ -20,4 +22,6 @@ const App = () => {
 	);
 }
 
+// * первый аргумент метода ReactDom.render() принимает исключительно react-элемент
+// * чтобы сделать react-элемент из компонента нужно прописать <.../>
 ReactDom.render(<App/>, document.getElementById('root'));
